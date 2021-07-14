2fe2cf4daab120509bc49add2afb87dd96145df8
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2021-07-14 09:20:53 -0700

    Initial

---
+++ tags/tag2.js
@@ -0,0 +1,3 @@
+function tag1(){
+    document.getElementById("mk4-tag1").innerHTML = '  <center><embed height="350mm" src="https://source.borgcreative.com/bct/mk2-bar-2.html" width="60%"></center>'
+}
