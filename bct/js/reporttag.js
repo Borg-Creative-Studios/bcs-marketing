6b564ad63daecdd392c09693980e3af6de0d5fdd
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2021-07-14 09:48:20 -0700

    bug fix

--- bct/js/reporttag.js
+++ bct/js/reporttag.js
@@ -1,3 +1,5 @@
 function reporttag(){
     document.getElementById("reportlink").href = 'https://www.borgcreative.com/adreport.html'
+
+    document.getElementById("reportlink1").href = 'https://www.borgcreative.com/adreport.html'
 }
