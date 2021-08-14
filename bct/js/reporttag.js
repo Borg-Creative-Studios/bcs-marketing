974e82947eb4f17d100a06199a55e090cf9a366c
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2021-08-13 23:09:05 -0700

    Update reporttag.js

--- bct/js/reporttag.js
+++ bct/js/reporttag.js
@@ -1,5 +1,5 @@
 function reporttag(){
-    document.getElementById("reportlink").href = 'https://www.borgcreative.com/adreport.html'
+    document.getElementById("reportlink").href = 'https://cloud.borgcreative.com/index.php/apps/forms/BSrfLdTJNKGHo8x2'
 
-    document.getElementById("reportlink1").href = 'https://www.borgcreative.com/adreport.html'
+    document.getElementById("reportlink1").href = 'https://cloud.borgcreative.com/index.php/apps/forms/BSrfLdTJNKGHo8x2'
 }
