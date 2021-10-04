6e1d152fdbb40242e06f2aebb5f4549855254f05
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2021-10-04 13:17:35 -0700

    The new ad report survey/form is live!

--- bct/js/reporttag.js
+++ bct/js/reporttag.js
@@ -1,14 +1,13 @@
+/*
 function reporttag(){
     document.getElementById("reportlink").href = 'https://www.borgcreative.com/adreport.html'
 
     document.getElementById("reportlink1").href = 'https://www.borgcreative.com/adreport.html'
 }
 
-/*
-function reporttag(){
-    document.getElementById("reportlink").href = 'https://cloud.borgcreative.com/index.php/apps/forms/BSrfLdTJNKGHo8x2'
-
-    document.getElementById("reportlink1").href = 'https://cloud.borgcreative.com/index.php/apps/forms/BSrfLdTJNKGHo8x2'
-}
-
 */
+function reporttag(){
+    document.getElementById("reportlink").href = 'https://cloud.borgcreative.com/survey/index.php/479343'
+
+    document.getElementById("reportlink1").href = 'https://cloud.borgcreative.com/survey/index.php/479343'
+}
